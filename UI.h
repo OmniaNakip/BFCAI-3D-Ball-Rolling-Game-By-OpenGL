cf8a46c165ff491d7522abbe5eaccdef578bb893
Abanoub  Rezk Gad <abanobrezk74@gmail.com>
2026-05-02 02:32:58 +0300

    Add UI system (Menu + About + Buttons)

---
+++ UI.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <GL/glut.h>
+#include <string>
+
+using namespace std;
+
+extern int screen;
+
+void drawMenu();
+void drawAbout();
+void mouse(int button, int state, int x, int y);
