cb27a82e6392bda3329b8de3aef24cdf423d8265
ahmed-hazem-1 <ahmed453189@fci.bu.edu.eg>
2026-05-01 03:12:49 +0300

    i edit the dir , and make it clean and make the game start

---
+++ src/Ground.cpp
@@ -0,0 +1,53 @@
+#include "Ground.h"
+#include <glad/glad.h>
+using namespace std;
+
+Ground::Ground(float size)
+{
+    float half = size / 2.0f;
+
+    // 4 corners of the ground on XZ plane (Y = 0)
+    float vertices[] = {
+        -half, 0.0f, -half,
+         half, 0.0f, -half,
+         half, 0.0f,  half,
+        -half, 0.0f,  half
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        0, 2, 3
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // location = 0 -> position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+}
+
+void Ground::Draw()
+{
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
+
+void Ground::CleanUp()
+{
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+}
